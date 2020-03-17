0e9e1fdb66191f04b2409278d8a01e05a061410c
Sindre Wilting <sindrewilting@gmail.com>
2020-03-17 14:17:43 +0100

    Stream processor id should have source microservice

--- Source/Events.Processing/Streams/StreamProcessorId.cs
+++ Source/Events.Processing/Streams/StreamProcessorId.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Dolittle.Applications;
 using Dolittle.Concepts;
 using Dolittle.Runtime.Events.Streams;
 
@@ -16,10 +17,12 @@ namespace Dolittle.Runtime.Events.Processing.Streams
         /// </summary>
         /// <param name="eventProcessorId"><see cref="EventProcessorId"/>.</param>
         /// <param name="sourceStreamId">The <see cref="StreamId"/>.</param>
-        public StreamProcessorId(EventProcessorId eventProcessorId, StreamId sourceStreamId)
+        /// <param name="sourceMicroservice">The source <see cref="SourceMicroservice" />.</param>
+        public StreamProcessorId(EventProcessorId eventProcessorId, StreamId sourceStreamId, Microservice sourceMicroservice)
         {
             EventProcessorId = eventProcessorId;
             SourceStreamId = sourceStreamId;
+            SourceMicroservice = sourceMicroservice;
         }
 
         /// <summary>
@@ -31,6 +34,11 @@ namespace Dolittle.Runtime.Events.Processing.Streams
         /// Gets or sets  the <see cref="StreamId" />.
         /// </summary>
         public StreamId SourceStreamId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source <see cref="SourceMicroservice" />.
+        /// </summary>
+        public Microservice SourceMicroservice { get; set; }
 
         /// <inheritdoc />
         public override string ToString()
