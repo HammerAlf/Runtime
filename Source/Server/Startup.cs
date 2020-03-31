7ecd9fc06ca61bed6e5caa5bed8fba5ab5fb84df
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-03-31 22:21:34 +0200

    Use new HostBuilder setup in Server

--- Source/Server/Startup.cs
+++ Source/Server/Startup.cs
@@ -1,51 +1,24 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Linq;
-using Autofac;
-using Dolittle.Booting;
-using Dolittle.DependencyInversion.Autofac;
-using Dolittle.DependencyInversion.Strategies;
-using Dolittle.Execution;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Dolittle.Runtime.Server
 {
     /// <summary>
     /// The startup for Asp.Net Core.
     /// </summary>
-    public class Startup : IDisposable
+    public class Startup
     {
-        readonly IWebHostEnvironment _env;
-        ILoggerFactory _loggerFactory;
-        BootloaderResult _bootResult;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Startup"/> class.
-        /// </summary>
-        /// <param name="env"><see cref="IWebHostEnvironment" />.</param>
-        public Startup(IWebHostEnvironment env)
-        {
-            _env = env;
-        }
-
         /// <summary>
         /// Configure all services.
         /// </summary>
         /// <param name="services"><see cref="IServiceCollection"/> to configure.</param>
         public void ConfigureServices(IServiceCollection services)
         {
-            _loggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder.AddConsole();
-                builder.AddEventSourceLogger();
-            });
-
             services.AddGrpc();
             services.AddGrpcWeb(_ => _.GrpcWebEnabled = true);
 
@@ -57,24 +30,9 @@ namespace Dolittle.Runtime.Server
                         .WithExposedHeaders("Grpc-Status", "Grpc-Message");
             }));
 
-            _bootResult = services.AddDolittle(
-                builder =>
-                {
-                    if (_env.IsDevelopment()) builder.Development();
-                },
-                _loggerFactory);
-            ManagementLogAppender.LoggerFactory = _loggerFactory;
-            var loggingBootStageResult = _bootResult.BootStageResults.ToArray()[1];
-            ManagementLogAppender.ExecutionContextManager = ((Constant)loggingBootStageResult.Bindings.First(_ => _.Service == typeof(IExecutionContextManager)).Strategy).Target as IExecutionContextManager;
-        }
-
-        /// <summary>
-        /// Configure the Autofac container.
-        /// </summary>
-        /// <param name="containerBuilder"><see cref="ContainerBuilder"/> to configure.</param>
-        public void ConfigureContainer(ContainerBuilder containerBuilder)
-        {
-            containerBuilder.AddDolittle(_bootResult.Assemblies, _bootResult.Bindings);
+            // ManagementLogAppender.LoggerFactory = _loggerFactory;
+            // var loggingBootStageResult = _bootResult.BootStageResults.ToArray()[1];
+            // ManagementLogAppender.ExecutionContextManager = ((Constant)loggingBootStageResult.Bindings.First(_ => _.Service == typeof(IExecutionContextManager)).Strategy).Target as IExecutionContextManager;
         }
 
         /// <summary>
@@ -84,8 +42,6 @@ namespace Dolittle.Runtime.Server
         /// <param name="env"><see cref="IWebHostEnvironment"/>.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDolittle();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -102,16 +58,11 @@ namespace Dolittle.Runtime.Server
             {
                 endpoints.MapGrpcService<Tenancy.Management.TenantsService>().EnableGrpcWeb().RequireCors("AllowAll");
                 endpoints.MapGrpcService<Heads.Management.HeadsService>().EnableGrpcWeb().RequireCors("AllowAll");
-                endpoints.MapGrpcService<Logging.Management.LogService>().EnableGrpcWeb().RequireCors("AllowAll");
+
+                // endpoints.MapGrpcService<Logging.Management.LogService>().EnableGrpcWeb().RequireCors("AllowAll");
             });
 
-            app.RunAsSinglePageApplication();
-        }
-
-        /// <inheritdoc/>
-        public void Dispose()
-        {
-            _loggerFactory.Dispose();
+            // app.RunAsSinglePageApplication();
         }
     }
 }
